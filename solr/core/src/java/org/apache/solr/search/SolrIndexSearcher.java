c44bec2a6adc7cae630b8804327064c24efddc81
patsonluk <patson@fullstory.com>
2025-06-30 16:47:33 -0700

    Fixed several edge cases (node creation/deletion) Added unit test case Added javadoc

--- solr/core/src/java/org/apache/solr/search/SolrIndexSearcher.java
+++ solr/core/src/java/org/apache/solr/search/SolrIndexSearcher.java
@@ -432,7 +432,8 @@ public class SolrIndexSearcher extends IndexSearcher implements Closeable, SolrI
   }
 
   @SuppressWarnings("unchecked")
-  private<K, V> SolrCache<K, V> buildCache(SolrConfig solrConfig, String cacheName, SolrCore core) {
+  private <K, V> SolrCache<K, V> buildCache(
+      SolrConfig solrConfig, String cacheName, SolrCore core) {
     CacheConfig cacheConfig;
     switch (cacheName) {
       case "fieldValueCache":
@@ -444,7 +445,7 @@ public class SolrIndexSearcher extends IndexSearcher implements Closeable, SolrI
       case "queryResultCache":
         cacheConfig = solrConfig.queryResultCacheConfig;
         break;
-      default: //generic caches
+      default: // generic caches
         cacheConfig = solrConfig.userCacheConfigs.get(cacheName);
     }
 
@@ -452,13 +453,11 @@ public class SolrIndexSearcher extends IndexSearcher implements Closeable, SolrI
       return null;
     }
 
-    //check overrides
-    List<Map<String, String>> overridesEntries = core.getCoreContainer().getCacheOverridesManager().getOverrides(cacheName, core.getCoreDescriptor().getCollectionName());
-    if (overridesEntries != null) {
-      for (Map<String, String> overrides : overridesEntries) {
-        cacheConfig = cacheConfig.withArgs(overrides);
-      }
-    }
+    // check overrides
+    cacheConfig =
+        core.getCoreContainer()
+            .getCacheOverridesManager()
+            .applyOverrides(cacheConfig, cacheName, core.getCoreDescriptor().getCollectionName());
 
     return (SolrCache<K, V>) cacheConfig.newInstance(core);
   }
