5d944421d17895ca3f991e8d94b1ee51e2a8db23
patsonluk <patson@fullstory.com>
2025-07-01 18:19:12 -0700

    tidy

--- solr/core/src/java/org/apache/solr/servlet/PrometheusMetricsServlet.java
+++ solr/core/src/java/org/apache/solr/servlet/PrometheusMetricsServlet.java
@@ -859,23 +859,23 @@ public final class PrometheusMetricsServlet extends BaseSolrServlet {
         "p99_ms",
         PrometheusMetricType.GAUGE),
     CUMULATIVE_DOCUMENT_CACHE_LOCAL_EVICTION(
-            "CACHE.searcher.documentCache",
-            "document_cache_store_local_eviction",
-            "Cumulative evictions from the per core local document cache store (vs backing shared cache store)",
-            "cumulative_evictions",
-            PrometheusMetricType.COUNTER),
+        "CACHE.searcher.documentCache",
+        "document_cache_store_local_eviction",
+        "Cumulative evictions from the per core local document cache store (vs backing shared cache store)",
+        "cumulative_evictions",
+        PrometheusMetricType.COUNTER),
     CUMULATIVE_FILTER_CACHE_LOCAL_EVICTION(
-            "CACHE.searcher.filterCache",
-            "filter_cache_store_local_eviction",
-            "Cumulative evictions from the per core local filter cache store (vs backing shared cache store)",
-            "cumulative_evictions",
-            PrometheusMetricType.COUNTER),
+        "CACHE.searcher.filterCache",
+        "filter_cache_store_local_eviction",
+        "Cumulative evictions from the per core local filter cache store (vs backing shared cache store)",
+        "cumulative_evictions",
+        PrometheusMetricType.COUNTER),
     CUMULATIVE_QUERY_RESULT_CACHE_LOCAL_EVICTION(
-            "CACHE.searcher.queryResultCache",
-            "query_result_cache_store_local_eviction",
-            "Cumulative evictions from the per core local query result cache store (vs backing shared cache store)",
-            "cumulative_evictions",
-            PrometheusMetricType.COUNTER);
+        "CACHE.searcher.queryResultCache",
+        "query_result_cache_store_local_eviction",
+        "Cumulative evictions from the per core local query result cache store (vs backing shared cache store)",
+        "cumulative_evictions",
+        PrometheusMetricType.COUNTER);
     final String key, metricName, desc, property;
     private final PrometheusMetricType metricType;
     private static final Map<String, CoreMetric> lookup = new HashMap<>();
@@ -1015,11 +1015,10 @@ public final class PrometheusMetricsServlet extends BaseSolrServlet {
       }
 
       String propertyClause =
-              properties.stream()
-                      .distinct()
-                      .map(p -> "&property=" + URLEncoder.encode(p, StandardCharsets.UTF_8))
-                      .collect(Collectors.joining());
-
+          properties.stream()
+              .distinct()
+              .map(p -> "&property=" + URLEncoder.encode(p, StandardCharsets.UTF_8))
+              .collect(Collectors.joining());
 
       return String.format(
           Locale.ROOT,
