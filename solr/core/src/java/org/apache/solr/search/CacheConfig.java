1e021b275663d98d0737ba0dd94f87734c1bb57e
Michael Gibney <michael@michaelgibney.net>
2025-08-18 14:27:46 -0400

    allow to pass args and relevant context to Regenerator ctors

--- solr/core/src/java/org/apache/solr/search/CacheConfig.java
+++ solr/core/src/java/org/apache/solr/search/CacheConfig.java
@@ -157,12 +157,23 @@ public class CacheConfig implements MapSerializable {
           }
         };
     config.regenImpl = config.args.get("regenerator");
-    if (config.regenImpl != null) {
-      config.regenerator = loader.newInstance(config.regenImpl, CacheRegenerator.class);
+    if (config.regenImpl != null && !config.regenImpl.isEmpty()) {
+      config.regenerator =
+          loader.newInstance(
+              config.regenImpl,
+              CacheRegenerator.class,
+              EMPTY,
+              REGEN_PARAMS,
+              new Object[] {solrConfig, config});
     }
 
     return config;
   }
+
+  private static final String[] EMPTY = new String[0];
+
+  @SuppressWarnings("rawtypes")
+  private static final Class<?>[] REGEN_PARAMS = new Class[] {SolrConfig.class, CacheConfig.class};
 
   @SuppressWarnings("rawtypes")
   public SolrCache newInstance(SolrCore core) {
