e1848cc683f71303eabac856ec02e40a4a22b073
patsonluk <patson@fullstory.com>
2025-09-12 12:14:19 -0700

    Logging on overrides change

--- solr/core/src/java/org/apache/solr/update/CommitTrackerManager.java
+++ solr/core/src/java/org/apache/solr/update/CommitTrackerManager.java
@@ -1,11 +1,15 @@
 package org.apache.solr.update;
 
+import java.lang.invoke.MethodHandles;
 import java.util.Map;
 import org.apache.solr.common.cloud.ClusterProperties;
 import org.apache.solr.common.cloud.ZkStateReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CommitTrackerManager {
-  public static final String ALIGN_COMMIT_TIME =
+  private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+  public static final String ALIGN_COMMIT_TIME_KEY =
       ClusterProperties.EXT_PROPRTTY_PREFIX + "alignCommitTime";
   private static volatile Boolean alignCommitTimeOverride;
 
@@ -14,7 +18,8 @@ public class CommitTrackerManager {
   public static void init(ZkStateReader zkStateReader) {
     zkStateReader.registerClusterPropertiesListener(
         (Map<String, Object> properties) -> {
-          alignCommitTimeOverride = (Boolean) properties.get(ALIGN_COMMIT_TIME);
+          alignCommitTimeOverride = (Boolean) properties.get(ALIGN_COMMIT_TIME_KEY);
+          log.info("{} change detected serving alignCommitTimeOverride: {}", ALIGN_COMMIT_TIME_KEY, alignCommitTimeOverride);
           return false;
         });
   }
