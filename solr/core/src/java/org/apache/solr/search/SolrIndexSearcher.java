6fe1219c1ed44106fb267d4d9d4b581784d877f2
patsonluk <patson@fullstory.com>
2026-02-06 18:45:04 -0800

    More debug statement for missing metrics

--- solr/core/src/java/org/apache/solr/search/SolrIndexSearcher.java
+++ solr/core/src/java/org/apache/solr/search/SolrIndexSearcher.java
@@ -1524,6 +1524,10 @@ public class SolrIndexSearcher extends IndexSearcher implements Closeable, SolrI
   public static void addCacheStats(
       Query key, boolean cacheHit, int docSetIdCount, long startTimeNanos) {
     SolrRequestInfo reqInfo = SolrRequestInfo.getRequestInfo();
+
+    log.info("Attempt to add cache stats for query: {}, cacheHit: {}, docSetIdCount: {}, elapsedMs: {}",
+        key, cacheHit, docSetIdCount,
+        java.util.concurrent.TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startTimeNanos));
     if (reqInfo != null && reqInfo.getResponseBuilder() != null) {
       org.apache.solr.common.util.NamedList<Object> stat =
           new org.apache.solr.common.util.SimpleOrderedMap<>();
@@ -1542,6 +1546,10 @@ public class SolrIndexSearcher extends IndexSearcher implements Closeable, SolrI
       stat.add("docSetIdCount", docSetIdCount);
 
       reqInfo.getResponseBuilder().addFilterStats(stat);
+
+      log.info("Added cache stats for query: {}, cacheHit: {}, docSetIdCount: {}, elapsedMs: {}",
+              key, cacheHit, docSetIdCount,
+              java.util.concurrent.TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startTimeNanos));
     }
   }
 
