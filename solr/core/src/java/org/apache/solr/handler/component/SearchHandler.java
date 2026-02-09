2fb57db85705a662111209ff9f4d5ae091d98de8
patsonluk <patson@fullstory.com>
2026-02-09 11:24:26 -0800

    More debug statement for missing metrics

--- solr/core/src/java/org/apache/solr/handler/component/SearchHandler.java
+++ solr/core/src/java/org/apache/solr/handler/component/SearchHandler.java
@@ -554,6 +554,10 @@ public class SearchHandler extends RequestHandlerBase
         if (!rb.getFilterStats()
             .isEmpty()) { // always attempt to add filter cache stats if possible
           NamedList<Object> headers = rb.rsp.getResponseHeader();
+          for (NamedList<Object> entry : rb.getFilterStats()) {
+            log.info("Adding filter stats to response and logs with entry {}", entry);
+          }
+
           if (headers != null) {
             headers.add("filtersStats", rb.getFilterStats());
           }
