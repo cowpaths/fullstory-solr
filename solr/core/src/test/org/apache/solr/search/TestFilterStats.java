2e4feb7b5c5e157eff448084eab5766e602929eb
patsonluk <patson@fullstory.com>
2026-02-19 14:48:51 -0800

    Fixed TestFilterStats

--- solr/core/src/test/org/apache/solr/search/TestFilterStats.java
+++ solr/core/src/test/org/apache/solr/search/TestFilterStats.java
@@ -17,8 +17,18 @@
 package org.apache.solr.search;
 
 import org.apache.solr.SolrTestCaseJ4;
+import org.apache.solr.common.util.NamedList;
+import org.apache.solr.request.SolrQueryRequest;
+import org.apache.solr.response.SolrQueryResponse;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.noggit.JSONParser;
+import org.noggit.ObjectBuilder;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
 
 public class TestFilterStats extends SolrTestCaseJ4 {
 
@@ -37,42 +47,76 @@ public class TestFilterStats extends SolrTestCaseJ4 {
 
     // Disable result cache on the main query using local params.
     // First request should MISS for both fqs (empty cache)
-    assertJQ(
-        req("q", "{!cache=false}*:*", "fq", "foo_s:a", "fq", "val_i:[1 TO 3]"),
-        "/responseHeader/filtersStats/[0]/key=='foo_s:a'",
-        "/responseHeader/filtersStats/[0]/time==\"///ignore///\"",
-        "/responseHeader/filtersStats/[0]/cacheHit==false",
-        "/responseHeader/filtersStats/[0]/docSetIdCount==2",
-        // using regex value below as the query class could be LegacyNumericRangeQuery,
-        // IndexOrDocValuesQuery
-        // or IntPoint$1, which give different toString values
-        "/responseHeader/filtersStats/[1]/key==\"///regex:.*val_i:\\\\[1 TO 3\\\\].*///\"",
-        "/responseHeader/filtersStats/[1]/time==\"///ignore///\"",
-        "/responseHeader/filtersStats/[1]/cacheHit==false",
-        "/responseHeader/filtersStats/[1]/docSetIdCount==3");
+    SolrQueryRequest req1 = req("q", "{!cache=false}*:*", "fq", "foo_s:a", "fq", "val_i:[1 TO 3]");
+    SolrQueryResponse rsp1 = h.queryAndResponse("/select", req1);
+    req1.close();
+
+    List<Map<String, Object>> filterStats1 = parseFilterStats(rsp1);
+    assertEquals(2, filterStats1.size());
+
+    Map<String, Object> filter1_0 = filterStats1.get(0);
+    assertEquals("foo_s:a", filter1_0.get("key"));
+    assertNotNull(filter1_0.get("time"));
+    assertEquals(false, filter1_0.get("cacheHit"));
+    assertEquals(2, ((Number) filter1_0.get("docSetIdCount")).intValue());
+
+    Map<String, Object> filter1_1 = filterStats1.get(1);
+    String key1_1 = (String) filter1_1.get("key");
+    assertTrue(key1_1.contains("val_i:[1 TO 3]"));
+    assertNotNull(filter1_1.get("time"));
+    assertEquals(false, filter1_1.get("cacheHit"));
+    assertEquals(3, ((Number) filter1_1.get("docSetIdCount")).intValue());
 
     // Second identical request should HIT for both
-    assertJQ(
-        req("q", "{!cache=false}*:*", "fq", "foo_s:a", "fq", "val_i:[1 TO 3]"),
-        "/responseHeader/filtersStats/[0]/key=='foo_s:a'",
-        "/responseHeader/filtersStats/[0]/time==\"///ignore///\"",
-        "/responseHeader/filtersStats/[0]/cacheHit==true",
-        "/responseHeader/filtersStats/[0]/docSetIdCount==2",
-        "/responseHeader/filtersStats/[1]/key==\"///regex:.*val_i:\\\\[1 TO 3\\\\].*///\"",
-        "/responseHeader/filtersStats/[1]/time==\"///ignore///\"",
-        "/responseHeader/filtersStats/[1]/cacheHit==true",
-        "/responseHeader/filtersStats/[1]/docSetIdCount==3");
+    SolrQueryRequest req2 = req("q", "{!cache=false}*:*", "fq", "foo_s:a", "fq", "val_i:[1 TO 3]");
+    SolrQueryResponse rsp2 = h.queryAndResponse("/select", req2);
+    req2.close();
 
-    // 3rd request, only 2nd fq is identical, so 1st should MISS, 2nd should HIT
-    assertJQ(
-        req("q", "{!cache=false}*:*", "fq", "foo_s:a", "fq", "val_i:[1 TO 2]"),
-        "/responseHeader/filtersStats/[0]/key=='foo_s:a'",
-        "/responseHeader/filtersStats/[0]/time==\"///ignore///\"",
-        "/responseHeader/filtersStats/[0]/cacheHit==true",
-        "/responseHeader/filtersStats/[0]/docSetIdCount==2",
-        "/responseHeader/filtersStats/[1]/key==\"///regex:.*val_i:\\\\[1 TO 2\\\\].*///\"",
-        "/responseHeader/filtersStats/[1]/time==\"///ignore///\"",
-        "/responseHeader/filtersStats/[1]/cacheHit==false",
-        "/responseHeader/filtersStats/[1]/docSetIdCount==2");
+    List<Map<String, Object>> filterStats2 = parseFilterStats(rsp2);
+    assertEquals(2, filterStats2.size());
+
+    Map<String, Object> filter2_0 = filterStats2.get(0);
+    assertEquals("foo_s:a", filter2_0.get("key"));
+    assertEquals(true, filter2_0.get("cacheHit"));
+    assertEquals(2, ((Number) filter2_0.get("docSetIdCount")).intValue());
+
+    Map<String, Object> filter2_1 = filterStats2.get(1);
+    String key2_1 = (String) filter2_1.get("key");
+    assertTrue(key2_1.contains("val_i:[1 TO 3]"));
+    assertEquals(true, filter2_1.get("cacheHit"));
+    assertEquals(3, ((Number) filter2_1.get("docSetIdCount")).intValue());
+
+    // 3rd request, only 2nd fq is identical, so 1st should HIT, 2nd should MISS
+    SolrQueryRequest req3 = req("q", "{!cache=false}*:*", "fq", "foo_s:a", "fq", "val_i:[1 TO 2]");
+    SolrQueryResponse rsp3 = h.queryAndResponse("/select", req3);
+    req3.close();
+
+    List<Map<String, Object>> filterStats3 = parseFilterStats(rsp3);
+    assertEquals(2, filterStats3.size());
+
+    Map<String, Object> filter3_0 = filterStats3.get(0);
+    assertEquals("foo_s:a", filter3_0.get("key"));
+    assertEquals(true, filter3_0.get("cacheHit"));
+    assertEquals(2, ((Number) filter3_0.get("docSetIdCount")).intValue());
+
+    Map<String, Object> filter3_1 = filterStats3.get(1);
+    String key3_1 = (String) filter3_1.get("key");
+    assertTrue(key3_1.contains("val_i:[1 TO 2]"));
+    assertEquals(false, filter3_1.get("cacheHit"));
+    assertEquals(2, ((Number) filter3_1.get("docSetIdCount")).intValue());
+  }
+
+  private List<Map<String, Object>> parseFilterStats(SolrQueryResponse rsp) throws Exception {
+    NamedList<Object> toLog = rsp.getToLog();
+    String filterStatsJson = (String) toLog.get("filtersStats");
+    assertNotNull("filtersStats should be present in toLog", filterStatsJson);
+
+    JSONParser parser = new JSONParser(filterStatsJson);
+    Object parsed = ObjectBuilder.getVal(parser);
+    assertTrue("filtersStats should be a list", parsed instanceof List);
+
+    @SuppressWarnings("unchecked")
+    List<Map<String, Object>> filterStats = (List<Map<String, Object>>) parsed;
+    return filterStats;
   }
 }
