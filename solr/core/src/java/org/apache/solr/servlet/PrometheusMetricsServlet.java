bc3675e7d0cf10fdb42e2a87fc4f57abaf1c1576
patsonluk <patson@fullstory.com>
2026-06-08 11:09:28 -0700

    Added metrics for merge durations

--- solr/core/src/java/org/apache/solr/servlet/PrometheusMetricsServlet.java
+++ solr/core/src/java/org/apache/solr/servlet/PrometheusMetricsServlet.java
@@ -706,6 +706,12 @@ public final class PrometheusMetricsServlet extends BaseSolrServlet {
   enum CoreMetric {
     MAJOR_MERGE(
         "INDEX.merge.major", "merges_major", "cumulative number of major merges across cores"),
+    MAJOR_MERGE_DURATION_P50(
+            "INDEX.merge.major", "merges_major_duration_p50", "p50 latency of major merges across cores", "median_ms", PrometheusMetricType.GAUGE),
+    MAJOR_MERGE_DURATION_P95(
+            "INDEX.merge.major", "merges_major_duration_p95", "p95 latency of major merges across cores", "p95_ms", PrometheusMetricType.GAUGE),
+    MAJOR_MERGE_DURATION_P99(
+            "INDEX.merge.major", "merges_major_duration_p99", "p99 latency of major merges across cores", "p99_ms", PrometheusMetricType.GAUGE),
     MAJOR_MERGE_RUNNING_DOCS(
         "INDEX.merge.major.running.docs",
         "merges_major_current_docs",
@@ -714,6 +720,13 @@ public final class PrometheusMetricsServlet extends BaseSolrServlet {
         PrometheusMetricType.GAUGE),
     MINOR_MERGE(
         "INDEX.merge.minor", "merges_minor", "cumulative number of minor merges across cores"),
+    MINOR_MERGE_DURATION_P50(
+            "INDEX.merge.minor", "merges_minor_duration_p50", "p50 latency of minor merges across cores", "median_ms", PrometheusMetricType.GAUGE),
+    MINOR_MERGE_DURATION_P95(
+            "INDEX.merge.minor", "merges_minor_duration_p95", "p95 latency of minor merges across cores", "p95_ms", PrometheusMetricType.GAUGE),
+    MINOR_MERGE_DURATION_P99(
+            "INDEX.merge.minor", "merges_minor_duration_p99", "p99 latency of minor merges across cores", "p99_ms", PrometheusMetricType.GAUGE),
+
     MINOR_MERGE_RUNNING_DOCS(
         "INDEX.merge.minor.running.docs",
         "merges_minor_current_docs",
