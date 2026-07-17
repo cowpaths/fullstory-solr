8c5ec2895b0ae526bfee4bed7202aeaf50dc7d53
Michael Gibney <michael@michaelgibney.net>
2026-07-17 15:29:54 -0400

    ensure enough space in initQueue

--- solr/core/src/java/org/apache/solr/storage/AccessDirectory2.java
+++ solr/core/src/java/org/apache/solr/storage/AccessDirectory2.java
@@ -187,7 +187,7 @@ public class AccessDirectory2 extends MMapDirectory {
     if (files == null) {
       files = new String[0];
     }
-    BlockingQueue<AD2IndexInput> initQueue = new ArrayBlockingQueue<>((files.length / 3) + 1);
+    BlockingQueue<AD2IndexInput> initQueue = new ArrayBlockingQueue<>(files.length + 1);
     Iterator<AD2IndexInput> initIter =
         new Iterator<AD2IndexInput>() {
           AD2IndexInput next;
